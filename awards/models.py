a0a6c4a9e848fabcde1a5ff7346921263abbb733
HassibIbrahim <ihassib@gmail.com>
2019-01-03 17:10:32 +0300

    voting system implemented

--- awards/models.py
+++ awards/models.py
@@ -66,6 +66,8 @@ class Profile(models.Model):
     name =models.CharField(max_length=100)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.name
 
 class Rating(models.Model):
     design = models.IntegerField(blank=True,default=0)
