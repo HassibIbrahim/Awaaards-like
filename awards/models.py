603aeb38ba78c6815c6e3feffaa2dea6d625c262
HassibIbrahim <ihassib@gmail.com>
2019-01-05 23:00:57 +0300

    implemented working tests

--- awards/models.py
+++ awards/models.py
@@ -2,37 +2,68 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from django.db.models import Q
- 
+
 import datetime as dt
 
 # Create your models here.
 class categories(models.Model):
     categories= models.CharField(max_length=100)
- 
+
     def __str__(self):
         return self.categories
- 
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def delete_category(cls,categories):
+        cls.objects.filter(categories=categories).delete()
+
+
 class technologies(models.Model):
     technologies = models.CharField(max_length=100)
- 
+
     def __str__(self):
         return self.technologies
- 
+
+    def save_technology(self):
+        self.save()
+
+    @classmethod
+    def delete_technology(cls,technologies):
+        cls.objects.filter(technologies=technologies).delete()
+
+
 class colors(models.Model):
     colors = models.CharField(max_length=100)
- 
+
     def __str__(self):
         return self.colors
- 
+
+    def save_color(self):
+        self.save()
+
+    @classmethod
+    def delete_color(cls,colors):
+        cls.objects.filter(colors=colors).delete()
+
 class countries(models.Model):
     countries = models.CharField(max_length=100)
- 
+
     def __str__(self):
         return self.countries
- 
+
     class Meta:
         ordering = ['countries']
- 
+
+
+    def save_country(self):
+        self.save()
+
+    @classmethod
+    def delete_country(cls,countries):
+        cls.objects.filter(countries=countries).delete()
+
 class Project(models.Model):
     title = models.CharField(max_length=150)
     landing_page = models.ImageField(upload_to='landingpage/')
@@ -54,17 +85,17 @@ class Project(models.Model):
     colors = models.ManyToManyField(colors)
     post_date = models.DateTimeField(auto_now_add=True)
     avatar = models.ImageField(upload_to='avatars/')
- 
+
     def __str__(self):
         return self.title
 
     @classmethod
     def search_project(cls,search_term):
         projects = cls.objects.filter(Q(username__username=search_term) | Q(title__icontains=search_term) | Q(colors__colors=search_term) | Q(technologies__technologies=search_term) | Q(categories__categories=search_term) | Q(country__countries=search_term))
- 
+
         return projects
- 
- 
+
+
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/')
     description = HTMLField()
